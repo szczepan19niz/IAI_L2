9a4f1474871166284ecfb49630d70c48ebddcc0f
szczepan19niz <30689@student.pwsztar.edu.pl>
2018-11-23 22:23:47 +0100

    added some improvements

--- js/navbar.js
+++ js/navbar.js
@@ -1,22 +1,22 @@
 
 let nav = '<nav>\n' +
     '<div id="header" class="container-fluid">' +
-    '<ul class="header_list">' +
-    '<div class="row">' +
-    '<div class="col-3">' +
-        '<li><a href="index.html">Strona główna</a></li>' +
-    '</div>'+
-    '<div class="col-3">'+
-        '<li><a href="kontakt.html">Kontakt</a></li>'+
-    '</div>'+
-    '<div class="col-3">'+
-        '<li><a href="map.html">Mapa</a></li>'+
-    '</div>'+
-    '<div class="col-3">'+
-        '<li><a href="quiz.html">Quiz</a></li>'+
-    '</div>'+
-    '</div>'+
-    '</ul>'+
+        '<ul class="header_list">' +
+            '<div class="row">' +
+                '<div class="col-3">' +
+                    '<li><a href="index.html">Strona główna</a></li>' +
+                '</div>'+
+                '<div class="col-3">'+
+                    '<li><a href="kontakt.html">Kontakt</a></li>'+
+                '</div>'+
+                '<div class="col-3">'+
+                    '<li><a href="map.html">Mapa</a></li>'+
+                '</div>'+
+                '<div class="col-3">'+
+                    '<li><a href="quiz.html">Quiz</a></li>'+
+                '</div>'+
+            '</div>'+
+        '</ul>'+
     '</div>'+
 '</nav>';
 
